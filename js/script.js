0a610015061eacd8ef278f76146c84c931ef7a5d
ynyanchoka <monaryymelda@gmail.com>
2022-03-10 19:08:54 +0300

    add toogle effect on what we do subheading

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    $("#design").click(function(){
+        $(".design-hidden").toggle();
+        $(".design-showing").toggle();
+    });
+
+    $("#development").click(function(){
+        $(".development-hidden").toggle();
+        $(".development-showing").toggle();
+      });
+
+    $("#product-management").click(function(){
+        $(".product-management-hidden").toggle();
+        $(".product-management-showing").toggle();
+    });
+
+   
+
+
+    });
