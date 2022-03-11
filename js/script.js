004a05ba11d3319101c586c24d1fec3a107565d8
ynyanchoka <monaryymelda@gmail.com>
2022-03-11 20:56:55 +0300

    add popup message

--- js/script.js
+++ js/script.js
@@ -22,22 +22,42 @@ $(document).ready(function() {
       function(){
         $(this).animate({opacity:'0'});
       })
-    $(document).ready(function(){
-        $("form#form34A").submit(function(event){
-          // event.preventDefault();
-          var name = $("input#MERGE1").val();
-          var email = $("input#MERGE0").val();
-          var message = $("textarea#comment").val();
-          if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-            alert (name + ", we have received your message. Thank you for reaching out to us.");
-          }
-          else {
-            alert("Please enter your name and email!");
-          }
-          
-        });
     
 
 
   });
+
+
+// $(document).ready(function(){
+//     $("#contactus").submit(function(event){
+          
+//       var name = $("input#name").val();
+//       var email = $("input#email").val();
+//       var message = $("textarea#message").val();
+//       if ($("input#name").val() && $("input#email").val()){
+//             alert (name + ", we have received your message. Thank you for reaching out to us.");
+//           }
+//       else {
+//             alert("Please enter your name and email!");
+//           }
+          
+//     });
+//   });
+
+
+  function validate () {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    if (name == "" || email  == ""|| message  ==""){
+      alert("Please enter your name, email and message!");
+       return false;
+
+    }
+    else{
+      alert (name + ", thank you for contacting us. We will get back to you as soon as possible.");
+          }
+    }
+      
+    
   
